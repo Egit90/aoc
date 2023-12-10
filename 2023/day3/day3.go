c578ac905e1868b62293d53f247ef82af2868b95
egit90 <elie90@gmail.com>
2023-12-09 19:26:44 -0500

    day3

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -10,54 +10,77 @@ import (
 	"github.com/elie90/aoc/types"
 )
 
-func Day3(partNumber types.PartNumber) {
-	file := files.NewFile("2023/day3/input")
-	read := file.ReadFile()
-
-	if types.One == partNumber {
-		play(read)
-	}
-
+type game struct {
+	puzzle        []string
+	listOfMatches map[int][][]int
+	startsIndex   map[int][][]int
+	res           int
 }
-
-func play(puzzle []string) {
-	res := 0
-	for lineIndex, line := range puzzle {
-
-		re := regexp.MustCompile(`[\d]+`)
-		matches := re.FindAllIndex([]byte(line), -1)
-
-		for _, match := range matches {
-			startIdx, endIdx := match[0], match[1]
-			if ok := checkIfShouldBeAdded(puzzle, lineIndex, startIdx, endIdx-1); ok {
-				substring := line[startIdx:endIdx]
-				fmt.Printf("Add Number : %s \n", substring)
-				if number, err := strconv.Atoi(substring); err == nil {
-					res += number
-				}
-
-			}
-		}
-
-	}
-
-	fmt.Println(res)
-}
-
-func isDot(s string, index int) bool {
-	if index < len(s) {
-		return string(s[index]) == "."
-	}
-
-	return true
-}
-
 type cords struct {
 	lineIndex int
 	charIndex int
 }
 
-func checkIfShouldBeAdded(puzzle []string, lineIndex int, leftIndex int, rightIndex int) bool {
+func Day3(partNumber types.PartNumber) {
+	file := files.NewFile("2023/day3/input")
+	read := file.ReadFile()
+
+	if types.One == partNumber {
+		p1 := &game{puzzle: read, listOfMatches: make(map[int][][]int)}
+		p1.getListOfNumbersAndIndexes()
+		p1.analyzeMatchesWithinLine()
+		fmt.Println(p1.res)
+		return
+	}
+
+	//part 2
+
+	p1 := &game{puzzle: read, listOfMatches: make(map[int][][]int)}
+	p1.getListOfNumbersAndIndexes()
+	p1.getListOfStarsIndexs()
+
+}
+
+func (p *game) getListOfNumbersAndIndexes() {
+	for lineIndex, line := range p.puzzle {
+		re := regexp.MustCompile(`[\d]+`)
+		matches := re.FindAllIndex([]byte(line), -1)
+		p.listOfMatches[lineIndex] = matches
+
+	}
+}
+
+func (p *game) getListOfStarsIndexs() {
+	for lineIndex, line := range p.puzzle {
+		re := regexp.MustCompile(`\*`)
+		matches := re.FindAllIndex([]byte(line), -1)
+		p.startsIndex[lineIndex] = matches
+
+	}
+}
+
+func (p *game) analyzeMatchesWithinLine() {
+
+	for lineIndex, matches := range p.listOfMatches {
+		for _, match := range matches {
+			startIdx, endIdx := match[0], match[1]
+			p.addToResult(lineIndex, startIdx, endIdx)
+		}
+	}
+
+}
+
+func (p *game) addToResult(lineIndex, startIdx, endIdx int) {
+	if p.checkIfShouldBeAdded(lineIndex, startIdx, endIdx-1) {
+		substring := p.puzzle[lineIndex][startIdx:endIdx]
+		if number, err := strconv.Atoi(substring); err == nil {
+			p.res += number
+		}
+
+	}
+}
+
+func (p *game) checkIfShouldBeAdded(lineIndex, leftIndex, rightIndex int) bool {
 
 	directions := map[string]cords{
 		"leftSide":    {lineIndex, leftIndex - 1},
@@ -78,7 +101,7 @@ func checkIfShouldBeAdded(puzzle []string, lineIndex int, leftIndex int, rightIn
 	}
 
 	for _, v := range directions {
-		if Char, err := getValueAtIndex(puzzle, v.lineIndex, v.charIndex); err == nil {
+		if Char, err := p.getValueAtIndex(v.lineIndex, v.charIndex); err == nil {
 			if isDot(Char, 0) {
 				continue
 			}
@@ -90,16 +113,31 @@ func checkIfShouldBeAdded(puzzle []string, lineIndex int, leftIndex int, rightIn
 	return false
 }
 
-func getValueAtIndex(puzzle []string, lineIndex int, index int) (string, error) {
+func (p *game) lineExists(line int) ([][]int, bool) {
+	if line > 0 && line < len(p.listOfMatches)-1 {
+		return p.listOfMatches[line], true
+	}
+	return nil, false
+}
 
-	if lineIndex < 0 || lineIndex >= len(puzzle) {
+func (p *game) getValueAtIndex(lineIndex, index int) (string, error) {
+
+	if lineIndex < 0 || lineIndex >= len(p.puzzle) {
 		return "", errors.New("out of bound")
 	}
 
-	s := puzzle[lineIndex]
+	s := (p.puzzle)[lineIndex]
 
 	if index < 0 || index >= len(s) {
 		return "", errors.New("out of bound")
 	}
 	return string(s[index]), nil
+}
+
+func isDot(s string, index int) bool {
+	if index < len(s) {
+		return string(s[index]) == "."
+	}
+
+	return true
 }
